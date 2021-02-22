ca8bc55432f361237876d7c3783f4a57c3da2ee1
hemanthharvard <hemanthharvard@gmail.com>
2021-02-21 19:20:19 -0800

    Added code

--- p1/script.js
+++ p1/script.js
@@ -2,15 +2,60 @@
 const Game = {
     data() {
         return {
-            userSelection: 'Rock'
+            isNewGame: true,
+            userSelection: "Rock",
+            computerSelection: this.makeComputerSelection(),
         }
     },
     methods: {
-        makeUserSelection() {
-            this.userSelection;
+        resetGame() {
+            this.isNewGame = false;
+        },
+        setUserSelection(item) {
+            this.userSelection = item;
+        },
+        makeComputerSelection() {
+            const myArray = [
+                "Rock",
+                "Paper",
+                "Scissor"
+              ];
+              return myArray[Math.floor(Math.random() * myArray.length)];
+        },
+        gameResult(playerChoice, computerChoice) {
+            let result = "win";
+    
+            switch(playerChoice) {
+                case "Rock":
+                    if(computerChoice === "Rock") {
+                        result = "tie";
+                    }
+                    else if(computerChoice === "Paper") {
+                        result = "lose";
+                    }
+                    break;
+                case "Paper":
+                    if(computerChoice === "Paper") {
+                        result = "tie";
+                    }
+                    else if(computerChoice === "Scissor") {
+                        result = "lose";
+                    }
+                    break;
+                case "Scissor":
+                    if(computerChoice === "Scissor") {
+                        result = "tie";
+                    }
+                    else if(computerChoice === "Rock") {
+                        result = "lose";
+                    }
+                    break;
+            }
+    
+            return result;
         }
     }
 }
 
 // Create a new Vue instance using our options
-const app = Vue.createApp(Game).mount('#app');
+const app = Vue.createApp(Game).mount("#app");
