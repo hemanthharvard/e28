a91488600fb235732697de25aa814dad89d286c3
hemanthharvard <hemanthharvard@gmail.com>
2021-04-11 20:21:28 -0700

    Update note.js

--- api/routes/note.js
+++ api/routes/note.js
@@ -5,7 +5,7 @@ const router = express.Router();
  * Test API
  */
 router.get('/apiTests', (req, res, next) => {
-	res.render('apiTests', {hostname: req.headers.host.split(':')[0], port: req.headers.host.split(':')[1]});
+	res.render('apiTests', {hostname: req.headers.host.split(':')[0], port: req.headers.host.split(':')[1] || 80});
 });
 
 module.exports = router;
