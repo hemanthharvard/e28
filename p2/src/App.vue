307c957484d531561769f8659542000fc967a734
hemanthharvard <hemanthharvard@gmail.com>
2021-04-15 23:00:59 -0700

    Added some CSS

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <div class="navbar">
-    <img
-      alt="Note Keeper"
-      id="logo"
-      class="logo"
-      src="@/assets/images/note-keeper.png"
-    />
-    <nav>
-      <ul>
-        <li>
-          <router-link
-            v-for="link in links"
-            v-bind:key="link"
-            v-bind:to="paths[link]"
-            >{{ link }}
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-
+  <div>
+    <div class="navbar">
+      <div class="navbar-header">
+        <img
+          alt="Note Keeper"
+          id="logo"
+          class="logo"
+          src="@/assets/images/note-keeper.png"
+        />
+        <h1>Note Keeper</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <router-link
+              v-for="link in links"
+              v-bind:key="link"
+              v-bind:to="paths[link]"
+              >{{ link }}
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
     <router-view
       v-bind:notes="notes"
       v-on:update-notes="loadNotes"
@@ -36,13 +40,13 @@ export default {
       notes: [],
 
       /* Store links in an array to maintain order */
-      links: ["home", "favourites", "new note"],
+      links: ["Home", "Favourites", "Create new note"],
 
       /* Map links to  the appropriate component */
       paths: {
-        home: "/",
-        favourites: "/favourites",
-        "new note": "/new",
+        Home: "/",
+        Favourites: "/favourites",
+        "Create new note": "/new",
       },
     };
   },
