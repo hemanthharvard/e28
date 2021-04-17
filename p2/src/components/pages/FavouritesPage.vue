36efc23596183c94d3e88643d11c6fec37a988b6
hemanthharvard <hemanthharvard@gmail.com>
2021-04-17 11:34:04 -0700

    Update FavouritesPage.vue

---
+++ p2/src/components/pages/FavouritesPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="favourites-page">
+    <ul>
+      <li v-for="note in notes" :key="note._id">
+        <note-card :label="note.title" v-bind:note="note"></note-card>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NoteCard from "@/components/NoteCard.vue";
+export default {
+  props: {
+    notes: {
+      type: Array,
+      default: null,
+    },
+  },
+  components: {
+    "note-card": NoteCard,
+  },
+};
+</script>
+
+<style scoped>
+#favourites-page {
+  padding: 2%;
+}
+#favourites-page > ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  list-style: none;
+}
+#favourites-page > ul > li {
+  padding: 2%;
+  min-width: 300px;
+  height: 30%;
+  width: 30%;
+}
+</style>
