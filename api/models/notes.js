ff0e41ec6708c6239c53774254d63debad5fc1c8
hemanthharvard <hemanthharvard@gmail.com>
2021-04-16 22:07:45 -0700

    Added isFavourite

--- api/models/notes.js
+++ api/models/notes.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const notesSchema = new Schema({
-    'created' : Date,
-    'updated' : Date,
-    'title': {type: String, required: true},
+    'created': Date,
+    'updated': Date,
+    'title': {
+        type: String,
+        required: true
+    },
+    'isFavourite': {
+        type: Boolean,
+        required: true
+    },
     'content': String,
-},
-{
+}, {
     strict: true,
     timestamps: true
 });
